d693b6b716a8852d0dfb0c6d56fdc24b78ef8c5a
Danveyd <danveydd@gmail.com>
2025-03-31 21:56:59 +0300

    Fix ShowInterfaceBrick

--- catroid/src/main/java/org/catrobat/catroid/content/actions/TestAction.kt
+++ catroid/src/main/java/org/catrobat/catroid/content/actions/TestAction.kt
@@ -7,19 +7,23 @@ import android.view.WindowInsets
 import com.badlogic.gdx.scenes.scene2d.actions.TemporalAction
 import org.catrobat.catroid.content.Scope
 import org.catrobat.catroid.formulaeditor.Formula
+import org.catrobat.catroid.stage.StageActivity
 
-class TestAction(private val activity: Activity) : TemporalAction() {
+class TestAction() : TemporalAction() {
     var scope: Scope? = null
 
     override fun update(percent: Float) {
         // Для Android 11 и выше (API 30+)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            activity.window.insetsController?.show(
-                WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars()
-            )
-        } else {
-            // Для старых версий Android (до API 30)
-            activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+        val activity = StageActivity.activeStageActivity.get()
+        activity?.runOnUiThread {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                activity.window.insetsController?.show(
+                    WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars()
+                )
+            } else {
+                // Для старых версий Android (до API 30)
+                activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+            }
         }
     }
 }
