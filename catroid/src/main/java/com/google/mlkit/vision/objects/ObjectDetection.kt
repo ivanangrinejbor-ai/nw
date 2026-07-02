9d6ad4f17d2bbbdd8618eab44b8c45da42f2afeb
ivanangrinejbor-ai <testoviiiakkk2@gmail.com>
2026-07-03 00:23:05 +0300

    fix: add import to ObjectDetection stub

--- catroid/src/main/java/com/google/mlkit/vision/objects/ObjectDetection.kt
+++ catroid/src/main/java/com/google/mlkit/vision/objects/ObjectDetection.kt
@@ -1,7 +1,9 @@
 package com.google.mlkit.vision.objects
 
+import com.google.mlkit.vision.objectdetection.ObjectDetector
+
 class ObjectDetection {
     companion object {
-        fun getClient(options: com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions): ObjectDetector = ObjectDetector()
+        fun getClient(options: com.google.mlkit.vision.objectdetection.defaults.ObjectDetectorOptions): ObjectDetector = ObjectDetector()
     }
 }
